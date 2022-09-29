3a3fb60b4e42e7056278de866c295d87cf2fcc17
Dan-Deama <109304725+Dan-Deama@users.noreply.github.com>
2022-09-29 14:05:07 -0300

    Add files via upload

---
+++ Módulo 1/Atividade Pacotes.py
@@ -0,0 +1,16 @@
+package br.com.matriz;
+public class Pessoas extends Pessoas
+{
+  public string estagiarios;
+  public string gerentes;
+  public string diretores;
+}
+
+package br.com.filial;
+
+public class Pessoas extends Pessoas
+{
+  public string gerentes;
+  public string areas;
+  public int salários;
+}
